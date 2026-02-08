5a7fce8c8522517256ef9366a395fb09dd2fa204
BaoPun <baophungus@gmail.com>
2026-02-08 12:45:52 -0800

    Added additional tests

--- tests/test_cstr.cpp
+++ tests/test_cstr.cpp
@@ -1,7 +1,4 @@
-#include <iostream>
-#include <cassert>
-#include <cstring>
-#include "cstr.hpp"
+#include "./cstr.hpp"
 
 int main() {
     // length()
@@ -48,7 +45,65 @@ int main() {
     assert(comp("abc", "abc") == 0);
     assert(comp("ab", "abc") == -1);
 
-    cout << "All tests passed." << endl;
+    // is_substr()
+    assert(is_substr("test", "this-is-a-test-string"));
+    assert(is_substr("beta", "betan"));
+    assert(!is_substr("test", "alpha"));
+    assert(is_substr("doctor", "doctor"));
+    assert(!is_substr("congratulations", "congrats"));
+
+    // is_palindrome()
+    assert(is_palindrome("racecar"));
+    assert(is_palindrome("lisabasil"));
+    assert(!is_palindrome("adam"));
+    assert(is_palindrome("aDamada"));
+
+    cout << "Congratulations, all tests have passed!" << endl;
+    cout << "Optionally, let's test the extra credit functions too" << endl;
+
+    // No asserts here, since these are extra credit.
+    // Just use if statements instead
+
+    // Testing count()
+    if(count("one two three four spaces", ' ') == 4){
+        cout << "ec test 1: count() is good" << endl;
+    }
+    else{
+        cout << "ec test 1: count() is incorrect" << endl;
+    }
+    if(count("what;are;spaces;doing;here", ' ') == 0){
+        cout << "ec test 2: count() is good" << endl;
+    }
+    else{
+        cout << "ec test 2: count() is incorrect" << endl;
+    }
+
+    // Testing tokenize()
+    char** tokens_1 = tokenize("this,is,a,test,string", ',');
+    bool tokens_1_correct = (
+        !comp(tokens_1[0], "this") &&
+        !comp(tokens_1[1], "is") &&
+        !comp(tokens_1[2], "a") &&
+        !comp(tokens_1[3], "test") &&
+        !comp(tokens_1[4], "string")
+    );
+    if(tokens_1_correct){
+        cout << "ec test 3: tokenize() is good" << endl;
+    }
+    else{
+        cout << "ec test 3: tokenize() is incorrect" << endl;
+    }
+    char** tokens_2 = tokenize("no-delimiters-here", ',');
+    bool tokens_2_correct = (
+        !comp(tokens_2[0], "no-delimiters-here")
+    );
+    if(tokens_2_correct){
+        cout << "ec test 4: tokenize() is good" << endl;
+    }
+    else{
+        cout << "ec test 4: tokenize() is incorrect" << endl;
+    }
+
     return 0;
 }
 
